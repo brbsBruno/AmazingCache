502b7e1283beb4159fa95d098a64a9ec44ef9436
Bruno Barbosa <barbosa.bruno@me.com>
2017-06-17 11:45:33 -0300

    Initial commit.

---
+++ AmazingCache/AmazingCache.h
@@ -0,0 +1,19 @@
+//
+//  AmazingCache.h
+//  AmazingCache
+//
+//  Created by Bruno Barbosa on 17/06/17.
+//  Copyright © 2017 Bruno Barbosa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AmazingCache.
+FOUNDATION_EXPORT double AmazingCacheVersionNumber;
+
+//! Project version string for AmazingCache.
+FOUNDATION_EXPORT const unsigned char AmazingCacheVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AmazingCache/PublicHeader.h>
+
+
