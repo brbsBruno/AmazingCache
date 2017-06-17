cee9aacc7db87bfeeb8328cb7c3a12e02664cb30
Bruno Barbosa <barbosa.bruno@me.com>
2017-06-17 11:55:05 -0300

    Include the first file to the framework

---
+++ AmazingCache/AmazingCache.swift
@@ -0,0 +1,16 @@
+//
+//  AmazingCache.swift
+//  AmazingCache
+//
+//  Created by Bruno Barbosa on 17/06/17.
+//  Copyright © 2017 Bruno Barbosa. All rights reserved.
+//
+
+import UIKit
+
+public class AmazingCache: NSObject {
+    
+    override init (){
+        print("AmazingCache has been initialised")
+    }
+}
