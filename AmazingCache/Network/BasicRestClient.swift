fa0c2b41ac0c3f689fd108c8a69879338cc2b2c0
Bruno Barbosa <barbosa.bruno@me.com>
2017-06-22 01:48:35 -0300

    Configure the URLSession to handle the completion in the main thread

--- AmazingCache/Network/BasicRestClient.swift
+++ AmazingCache/Network/BasicRestClient.swift
@@ -14,11 +14,9 @@ final class BasicRestClient: NSObject {
      https://talk.objc.io/episodes/S01E01-networking
      */
     func get<A>(_ resource: Resource<A>, completion: @escaping (A?, Error?) -> ()) {
-        URLSession.shared.dataTask(with: resource.url) { data, _, error in
+        URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main).dataTask(with: resource.url) { data, _, error in
             let result = data.flatMap(resource.decode)
-            DispatchQueue.main.async {
                 completion(result, error)
-            }
             }.resume()
     }
 }
